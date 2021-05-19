08eae51233b4a7b84f0f63be9c6bf1e4e5eefb1e
Regan Willis <reganwillis@outlook.com>
2021-05-19 13:13:32 -0500

    better multiple json response handling, adds comments and posts

--- RESTClient.js
+++ RESTClient.js
@@ -1,8 +1,7 @@
 // Regan Willis 2021
 // REST Client
 
-// TODO: documentation - posts
-// TODO: resources - comments, albums, photos, todos, users
+// TODO: resources - albums, photos, todos, users
 
 
 /**
@@ -143,6 +142,162 @@ export class Posts {
    */
   static delete(host, id) {
     let url = `${host}/posts/${id}`;
+
+    return delete_data(url);
+  }
+}
+
+
+/**
+ * Accesses the server comments
+ * @class
+ */
+ export class Comments {
+
+  /**
+   * Gets comment information
+   * @method
+   * @param {string} host - URL where the server is hosted
+   * @param {?string} id - id of a specific comment
+   * @return {JSON} Specified comment or list of comments
+   */
+  static get(host, id=null) {
+    let url = `${host}/comments`;
+
+    if (id != null) {
+      url += `/${id}`;
+    }
+
+    return get_data(url);
+  }
+
+  /**
+   * Posts a comment
+   * @method
+   * @param {string} host - URL where the server is hosted
+   * @param {string} name - name attached to comment
+   * @param {string} email - email attached to comment
+   * @param {string} body - body of comment
+   * @return {JSON} Response from server
+   */
+  static post(host, name, email, body) {
+    let url = `${host}/comments`;
+
+    let payload = {
+      'name': name,
+      'email': email,
+      'body': body,
+    }
+
+    return send_data(url, 'POST', payload);
+  }
+
+  /**
+   * Updates a comment
+   * @param {string} host - URL where the server is hosted
+   * @param {string} id - id of a specific comment
+   * @param {?string} name - updated name attached to comment
+   * @param {?string} email - updated email attached to comment
+   * @param {?string} body - updated body of comment
+   * @return {JSON} Response from server
+   */
+  static put(host, id, name=null, email=null, body=null) {
+    let url = `${host}/comments/${id}`;
+    let payload = {};
+
+    if (name != null)
+      payload['name'] = name;
+
+    if (email != null)
+      payload['email'] = email;
+
+    if (body != null)
+      payload['body'] = body;
+
+    return send_data(url, 'PUT', payload);
+  }
+
+  /**
+   * Deletes comments
+   * @method
+   * @param {string} host - URL where the server is hosted
+   * @param {string} id - id of a specific comment
+   * @return {JSON} Response from server
+   */
+  static delete(host, id) {
+    let url = `${host}/comments/${id}`;
+
+    return delete_data(url);
+  }
+}
+
+
+/**
+ * Accesses the server albums
+ * @class
+ */
+ export class Albums {
+
+  /**
+   * Gets album information
+   * @method
+   * @param {string} host - URL where the server is hosted
+   * @param {?string} id - id of a specific album
+   * @return {JSON} Specified album or list of albums
+   */
+  static get(host, id=null) {
+    let url = `${host}/albums`;
+
+    if (id != null) {
+      url += `/${id}`;
+    }
+
+    return get_data(url);
+  }
+
+  /**
+   * Posts an album
+   * @method
+   * @param {string} host - URL where the server is hosted
+   * @param {string} title - title of album
+   * @return {JSON} Response from server
+   */
+  static post(host, title) {
+    let url = `${host}/albums`;
+
+    let payload = {
+      'title': title,
+    }
+
+    return send_data(url, 'POST', payload);
+  }
+
+  /**
+   * Updates an album
+   * @param {string} host - URL where the server is hosted
+   * @param {string} id - id of a specific album
+   * @param {?string} title - updated title of album
+   * @return {JSON} Response from server
+   */
+  static put(host, id, title=null) {
+    let url = `${host}/albums/${id}`;
+    let payload = {};
+
+    if (title != null)
+      payload['title'] = title;
+
+    return send_data(url, 'PUT', payload);
+  }
+
+  /**
+   * Deletes albums
+   * @method
+   * @param {string} host - URL where the server is hosted
+   * @param {string} id - id of a specific album
+   * @return {JSON} Response from server
+   */
+  static delete(host, id) {
+    let url = `${host}/albums/${id}`;
 
     return delete_data(url);
   }
--- index.js
+++ index.js
@@ -6,23 +6,48 @@ import * as RESTClient from './RESTClient.js'
 
 let host = 'https://jsonplaceholder.typicode.com';
 
-RESTClient.Posts.get(host, 3).then(json => parse_posts(json));
-RESTClient.Posts.post(host, "add title", "add body").then(json => parse_posts(json));
-RESTClient.Posts.put(host, 1, null, "update body").then(json => parse_posts(json));
-RESTClient.Posts.delete(host, 2).then(response => handle_deletes(response));
+// interact with posts
+RESTClient.Posts.get(host, 1).then(json => handle_get_response(json, 'posts'));
+RESTClient.Posts.post(host, "add title", "add body").then(json => parse_post(json));
+RESTClient.Posts.put(host, 2, null, "update body").then(json => parse_post(json));
+RESTClient.Posts.delete(host, 3).then(response => handle_deletes(response));
 
-function parse_posts(json) {
+// interact with comments
+RESTClient.Comments.get(host, 1).then(json => handle_get_response(json, 'comments'));
+RESTClient.Comments.post(host, "add name", "add email", "add body").then(json => parse_comment(json));
+RESTClient.Comments.put(host, 2, "update name").then(json => parse_comment(json));
+RESTClient.Comments.delete(host, 3).then(json => handle_deletes(json));
+
+// interact with albums
+RESTClient.Albums.get(host, 1).then(json => handle_get_response(json, 'albums'));
+RESTClient.Albums.post(host, "add title").then(json => parse_album(json));
+RESTClient.Albums.put(host, 2, "update title").then(json => parse_album(json));
+RESTClient.Albums.delete(host, 3).then(json => handle_deletes(json));
+
+// handle json response as array or individual element
+function handle_get_response(json, type) {
 
     if (Array.isArray(json)) {
 
         for (let elem of json) {
-            parse_post(elem);
+            match_type(elem, type);
         }
     } else {
-        parse_post(json);
+        match_type(json, type);
     }
 }
 
+// send data to correct parsing function based on type
+function match_type(json, type) {
+    if (type == 'posts')
+        parse_post(json);
+    else if (type == 'comments')
+        parse_comment(json);
+    else if (type == 'albums')
+        parse_album(json);
+}
+
+// parse individual post and print
 function parse_post(json) {
     let str = JSON.stringify(json);
     let id = JSON.parse(str).id;
@@ -34,6 +59,31 @@ function parse_post(json) {
     console.log(body);
 }
 
+// parse individual comment and print
+function parse_comment(json) {
+    let str = JSON.stringify(json);
+    let id = JSON.parse(str).id;
+    let name = JSON.parse(str).name;
+    let email = JSON.parse(str).email;
+    let body = JSON.parse(str).body;
+
+    console.log(id);
+    console.log(name);
+    console.log(email);
+    console.log(body);
+}
+
+// parse individual album and print
+function parse_album(json) {
+    let str = JSON.stringify(json);
+    let id = JSON.parse(str).id;
+    let title = JSON.parse(str).title;
+
+    console.log(id);
+    console.log(title);
+}
+
+// print appropriate delete response
 function handle_deletes(response) {
     let message = "";
     let str = JSON.stringify(response);
